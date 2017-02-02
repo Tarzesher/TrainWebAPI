918c9a99dd4d566f570bcbe621b0d4d7fe41d5c4
Tarzesher <Tarzesher@gmail.com>
2017-02-02 10:47:25 +0200

    Creating some dependacy through ninject

---
+++ TrainWebAPI/Models/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace TrainWebAPI.Models
+{
+    public interface IRepository
+    {
+        IQueryable<Order> GetAllOrders();
+        IQueryable<Order> GetAllOrderswithDetailsOrders();
+        Order GetOrder(int id);
+    }
+}
---
+++ TrainWebAPI/Models/Repository.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Linq;
+
+namespace TrainWebAPI.Models
+{
+    public class Repository : IRepository
+    {
+        private TrainWebAPIContext db;
+
+        public Repository(TrainWebAPIContext db)
+        {
+            this.db = db;
+        }
+
+        public IQueryable<Order> GetAllOrders()
+        {
+            return db.Orders;
+        }
+
+        public IQueryable<Order> GetAllOrderswithDetailsOrders()
+        {
+            return db.Orders.Include("OrderDetails");
+        }
+
+        public Order GetOrder(int id)
+        {
+            return db.Orders.Include("OrderDetails.Book")
+                .FirstOrDefault(o => o.Id == id);
+        }
+    }
+}
